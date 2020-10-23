9acd61698ebb2b00e867fa46c738d8952ee73242
Ryan T <ryantmk518@gmail.com>
2020-10-23 16:13:18 -0700

    Gp theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,32 @@
-!(function ($) {
+/**
+* Template Name: Gp - v2.1.0
+* Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
   "use strict";
 
+  // Preloader
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
+
   // Smooth scroll for the navigation menu and links with .scrollto classes
-  var scrolltoOffset = $('#header').outerHeight() - 1;
+  var scrolltoOffset = $('#header').outerHeight() - 2;
   $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      e.preventDefault();
       var target = $(this.hash);
       if (target.length) {
-        e.preventDefault();
-        
+
         var scrollto = target.offset().top - scrolltoOffset;
 
-        if( $(this).attr("href") == '#header') {
+        if ($(this).attr("href") == '#header') {
           scrollto = 0;
         }
 
@@ -35,7 +50,7 @@
   });
 
   // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function(){
+  $(document).ready(function() {
     if (window.location.hash) {
       var initial_nav = window.location.hash;
       if ($(initial_nav).length) {
@@ -61,7 +76,7 @@
       $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
       $('.mobile-nav-overly').toggle();
     });
-    
+
     $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
       e.preventDefault();
       $(this).next().slideToggle(300);
@@ -86,20 +101,20 @@
   var nav_sections = $('section');
   var main_nav = $('.nav-menu, #mobile-nav');
 
-  $(window).on('scroll', function () {
+  $(window).on('scroll', function() {
     var cur_pos = $(this).scrollTop() + 200;
-  
+
     nav_sections.each(function() {
       var top = $(this).offset().top,
-          bottom = top + $(this).outerHeight();
-  
+        bottom = top + $(this).outerHeight();
+
       if (cur_pos >= top && cur_pos <= bottom) {
-        if( cur_pos <= bottom ) {
+        if (cur_pos <= bottom) {
           main_nav.find('li').removeClass('active');
         }
-        main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
       }
-      if( cur_pos < 300 ) {
+      if (cur_pos < 300) {
         $(".nav-menu ul:first li:first").addClass('active');
       }
     });
@@ -118,16 +133,8 @@
     $('#header').addClass('header-scrolled');
   }
 
-  // Intro carousel
-  var heroCarousel = $("#heroCarousel");
-
-  heroCarousel.on('slid.bs.carousel', function (e) {
-    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
-    $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
-  });
-
   // Back to top button
-  $(window).scroll(function () {
+  $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
     } else {
@@ -135,31 +142,59 @@
     }
   });
 
-  $('.back-to-top').click(function () {
+  $('.back-to-top').click(function() {
     $('html, body').animate({
       scrollTop: 0
     }, 1500, 'easeInOutExpo');
     return false;
   });
 
+  // Clients carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 6
+      }
+    }
+  });
+
   // Porfolio isotope and filter
-  $(window).on('load', function () {
+  $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
       itemSelector: '.portfolio-item'
     });
 
-    $('#portfolio-flters li').on( 'click', function() {
+    $('#portfolio-flters li').on('click', function() {
       $("#portfolio-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
 
-      portfolioIsotope.isotope({ filter: $(this).data('filter') });
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
       aos_init();
     });
 
     // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function(){
-      $('.venobox').venobox(); 
+    $(document).ready(function() {
+      $('.venobox').venobox({
+        'share': false
+      });
     });
+  });
+
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
   });
 
   // Testimonials carousel (uses the Owl Carousel library)
@@ -167,9 +202,9 @@
     autoplay: true,
     dots: true,
     loop: true,
-    responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 3 } }
+    items: 1
   });
-  
+
   // Portfolio details carousel
   $(".portfolio-details-carousel").owlCarousel({
     autoplay: true,
@@ -182,9 +217,7 @@
   function aos_init() {
     AOS.init({
       duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false
+      once: true
     });
   }
   $(window).on('load', function() {
